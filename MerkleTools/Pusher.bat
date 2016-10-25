5df33aaa343f2bcd36526001e48e41a1ec3abd82
lontivero <lucasontivero@gmail.com>
2016-10-25 15:06:17 -0300

    add metadata to packOptions

--- MerkleTools/Pusher.bat
+++ MerkleTools/Pusher.bat
@@ -4,9 +4,9 @@ cd %~dp0
 set /p version= Version? 
 
 echo Packing
-dotnet pack -c MerkleTools.nuspec --version-suffix %version%
+dotnet pack --version-suffix %version%
 echo Pushing to nuget.org
 set /p apikey= API Key? 
-..\.nuget\nuget push MerkleTools.%version%.nupkg -s https://nuget.org/ -ApiKey %apikey%
+..\.nuget\nuget push bin\MerkleTools.nuspec\MerkleTools.1.0.0-%version%.nupkg -s https://nuget.org/ -ApiKey %apikey%
 echo Done!
 pause
