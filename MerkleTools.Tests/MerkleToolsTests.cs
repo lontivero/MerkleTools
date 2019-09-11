300fb80a4827e1b3858258183d02dc808b9dd084
lontivero <lucasontivero@gmail.com>
2019-09-11 15:18:53 -0300

    Fix broken tests

--- MerkleTools.Tests/MerkleToolsTests.cs
+++ MerkleTools.Tests/MerkleToolsTests.cs
@@ -44,7 +44,7 @@ namespace MerkleTools.Tests
 			{
 				mt.AddLeaf(Encoding.ASCII.GetBytes($"test{i}"), true);
 			}
-			Assert.Equal(5, mt.Levels);
+			Assert.Equal(4, mt.Levels);
 
 			var proof = mt.GetProof(0);
 			Assert.Equal("right:1b4f0e9851971998e732078544c96b36c3d01cedf7caa332359d6f1d83567014", proof[0].ToString());
@@ -75,7 +75,7 @@ namespace MerkleTools.Tests
 			{
 				mt.AddLeaf(sha256($"test{i}"));
 			}
-			Assert.Equal(5, mt.Levels);
+			Assert.Equal(4, mt.Levels);
 
 			foreach (var i in Enumerable.Range(0, 10))
 			{
@@ -98,7 +98,7 @@ namespace MerkleTools.Tests
 			{
 				mt.AddLeaf(sha256($"test{i}"));
 			}
-			Assert.Equal(5, mt.Levels);
+			Assert.Equal(4, mt.Levels);
 
 			foreach (var i in Enumerable.Range(0, 10))
 			{
